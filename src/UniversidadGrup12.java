84c3c004ce0bc5684972547ef7ea38f5ec91f274
PascualiDarius12 <dario12bifa@gmail.com>
2023-05-20 17:20:32 -0300

    Initial commit

---
+++ src/UniversidadGrup12.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 54266
+ */
+public class UniversidadGrup12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
